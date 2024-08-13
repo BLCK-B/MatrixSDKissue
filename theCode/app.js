619643335e0d0c761c085a0c483c08dfaa128206
BLCK <123077751+BLCK-B@users.noreply.github.com>
2024-08-13 12:42:36 +0200

    init

---
+++ theCode/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import * as sdk from "matrix-js-sdk";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
